482b6753a63d6365a835bb453f1e365fc7455fb7
Andreas Trøstrup <andreastrstrup@gmail.com>
2023-06-30 00:07:02 +0200

    Add mobility gem

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module Gaerdesmutte
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.i18n.available_locales = %i[da en]
   end
 end
