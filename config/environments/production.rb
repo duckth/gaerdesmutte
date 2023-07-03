3adca76d49c1914f2772d0507ef6d0bad9300d05
Andreas Trøstrup <andreastrstrup@gmail.com>
2023-07-03 11:30:45 +0200

    Update hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.hosts << "gaerdesmutte.northeurope.cloudapp.azure.com"
 end
