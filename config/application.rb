1961a9da05f45ab9dababfabf2435fbadf20eed4
Andreas Trøstrup <andreastrstrup@gmail.com>
2023-07-11 19:42:53 +0200

    Re-initialize app

--- config/application.rb
+++ config/application.rb
@@ -3,15 +3,15 @@ require_relative "boot"
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-# require "active_job/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
 # require "action_mailer/railtie"
 # require "action_mailbox/engine"
-# require "action_text/engine"
+require "action_text/engine"
 require "action_view/railtie"
-# require "action_cable/engine"
+require "action_cable/engine"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -30,11 +30,6 @@ module Gaerdesmutte
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    config.api_only = false
     config.i18n.available_locales = %i[da en]
   end
 end
--- test/controllers/facts_controller_test.rb
+++ test/controllers/facts_controller_test.rb
@@ -2,12 +2,21 @@ require "test_helper"
 
 class FactsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+<<<<<<< HEAD
     get "/facts.json"
     assert_response :success
   end
 
   test "should get random" do
     get "/facts/random.json"
+=======
+    get facts_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get facts_show_url
+>>>>>>> 1e9c430 (Re-initialize app)
     assert_response :success
   end
 end
