7ee40da8082c660936ad0e6545fe9d59cd93b5ae
Andreas Trøstrup <andreastrstrup@gmail.com>
2023-07-18 16:12:20 +0200

    fix before action

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,7 +1,4 @@
 class Api::ApiController < ApplicationController
-  skip_before_action :logged_in_using_omniauth?
-  skip_before_action :set_current_user
-
   around_action :switch_locale
 
   def switch_locale(&)
@@ -10,6 +7,6 @@ class Api::ApiController < ApplicationController
   end
 
   def default_url_options
-    {locale: I18n.locale}
+    { locale: I18n.locale }
   end
 end
