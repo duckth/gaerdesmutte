50554a630513319a7be953640fdd86923b0c2ed6
Andreas Trøstrup <andreastrstrup@gmail.com>
2023-07-18 16:14:29 +0200

    maybe fix

--- app/controllers/api/facts_controller.rb
+++ app/controllers/api/facts_controller.rb
@@ -1,8 +1,8 @@
-class Api::FactsController < ApiController
+class Api::FactsController < Api::ApiController
   def index
     @fact = Fact.all.sample
     @photo = @fact.photos.sample
-    @with_image = params[:image] == "true"
-    @with_species_info = params[:species_info] == "true"
+    @with_image = params[:image] == 'true'
+    @with_species_info = params[:species_info] == 'true'
   end
 end
