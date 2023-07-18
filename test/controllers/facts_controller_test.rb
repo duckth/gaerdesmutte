3ebb215960a441f2653bcde5254048a231d02b9e
Andreas Trøstrup <andreastrstrup@gmail.com>
2023-07-18 14:48:57 +0200

    fix merge conflct

--- test/controllers/facts_controller_test.rb
+++ test/controllers/facts_controller_test.rb
@@ -1,22 +1,13 @@
-require "test_helper"
+require 'test_helper'
 
 class FactsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-<<<<<<< HEAD
-    get "/facts.json"
-    assert_response :success
-  end
-
-  test "should get random" do
-    get "/facts/random.json"
-=======
+  test 'should get index' do
     get facts_index_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get facts_show_url
->>>>>>> 1e9c430 (Re-initialize app)
     assert_response :success
   end
 end
