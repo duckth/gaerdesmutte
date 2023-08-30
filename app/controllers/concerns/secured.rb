36bdb13a8b14a9f34b28efd0b5efff9630cc1141
Andreas Trøstrup <andreastrstrup@gmail.com>
2023-08-30 16:23:57 +0200

    Skip auth if running in dev env (#5)
    * Skip auth if running in dev env
    
    * Move check to function

--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -13,14 +13,22 @@ module Secured
   end
 
   def logged_in_using_omniauth?
+    return if Rails.env.development?
+
     redirect_to "/" unless session[:userinfo].present?
   end
 
   def set_current_user
+    return set_mock_user if Rails.env.development?
+
     @current_user = User.new(
       session[:userinfo]["nickname"],
       session[:userinfo]["name"],
       session[:userinfo]["picture"]
     )
+  end
+
+  def set_mock_user
+    @current_user = User.new("testuser", "test", nil)
   end
 end
