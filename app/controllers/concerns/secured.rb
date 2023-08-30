b6608e6351236e11bdb0ff37be0fc1bd2ab64b74
Andreas Trøstrup <andreastrstrup@gmail.com>
2023-08-30 13:45:23 +0200

    Skip auth if running in dev env

--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -8,7 +8,7 @@ module Secured
   )
 
   included do
-    before_action :logged_in_using_omniauth?
+    before_action :logged_in_using_omniauth?, if: :production?
     before_action :set_current_user
   end
 
@@ -17,10 +17,20 @@ module Secured
   end
 
   def set_current_user
+    return set_mock_user if Rails.env.development?
+
     @current_user = User.new(
       session[:userinfo]["nickname"],
       session[:userinfo]["name"],
       session[:userinfo]["picture"]
     )
+  end
+
+  def set_mock_user
+    @current_user = User.new("testuser", "test", nil)
+  end
+
+  def production?
+    Rails.env.production?
   end
 end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,6 +1,7 @@
 class LoginController < ApplicationController
   before_action :redirect_if_logged_in
-  
+  before_action :redirect_if_development
+
   def index
   end
 
@@ -8,5 +9,9 @@ class LoginController < ApplicationController
 
   def redirect_if_logged_in
     redirect_to admin_interface_url if session[:userinfo].present?
+  end
+
+  def redirect_if_development
+    redirect_to admin_interface_url if Rails.env.development?
   end
 end
